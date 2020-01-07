6469c9b58ece2206c3d1955af1e6738063287f5f
Richard Ma <richard.ma.19850509@gmail.com>
2020-01-07 14:06:25 +0800

    use fake database

--- Gondwana/tests/conftest.py
+++ Gondwana/tests/conftest.py
@@ -2,20 +2,44 @@
 # encoding: utf-8
 
 import os
+import tempfile
 import pytest
 
 from Gondwana import create_app
+from Gondwana.model import db
 
 
+# create app
 @pytest.fixture
 def app():
     # set env to testing
     os.environ['FLASK_ENV'] = 'testing'
-    app = create_app()
 
-    yield app
+    # creaate fake database
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///' + db_path,
+    })
+
+    # import test data
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+    # remove fake database
+    os.close(db_fd)
+    os.unlink(db_path)
 
 
+# create test client
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+# create test command line runner
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
