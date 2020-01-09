d97193a35f2b06dddba74e34224c1e98eb645000
Richard Ma <richard.ma.19850509@gmail.com>
2020-01-09 09:35:34 +0800

    add channel model

--- Gondwana/model.py
+++ Gondwana/model.py
@@ -10,6 +10,20 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 # models
+class Channel(db.Model):
+    __tablename__ = 'channel'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), unique=True)
+    website_url = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), nullable=False)
+    api_key = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, name, website_url, email, api_key):
+        self.name = name
+        self.website_url = website_url
+        self.email = email
+        self.api_key = api_key
 
 # command line
 @click.command('init-db')
