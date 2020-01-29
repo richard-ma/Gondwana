65112985fbee26625d429ff5e1ba2793498f656d
Richard Ma <richard.ma.19850509@gmail.com>
2020-01-29 11:05:23 +0800

    add post to channel_create

--- Gondwana/channel.py
+++ Gondwana/channel.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, current_app
 
 bp = Blueprint('channel', __name__, url_prefix='/channel')
+
 
 # /channel/index
 @bp.route('/index', methods=('GET', 'POST'))
@@ -13,13 +14,17 @@ def channel_index():
 
     return render_template('channel/index.html', active_page="channel_index")
 
+
 # /channel/create
 @bp.route('/create', methods=('GET', 'POST'))
 def channel_create():
     if request.method == 'POST':
-        pass
+        current_app.logger.debug('create channel')
+
+        return redirect(url_for('channel.channel_index'))
 
     return render_template('channel/create.html')
+
 
 # /channel/delete/<str:id>
 @bp.route('/delete/<string:id>', methods=('GET', 'POST'))
@@ -27,7 +32,7 @@ def channel_delete(id: str):
     if request.method == 'POST':
         pass
 
-    return redirect(url_for('channel.index'))
+    return redirect(url_for('channel.channel_index'))
 
 
 # /channel/update/<int:id>
@@ -37,6 +42,7 @@ def channel_update(id: str):
         pass
 
     return render_template('channel/update.html')
+
 
 # /channel/info/<int:id>
 @bp.route('/info/<string:id>', methods=('GET', 'POST'))
