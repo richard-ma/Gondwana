ffe57f6cd60cf20436b8fd4172b96d2afa371ac3
Richard Ma <richard.ma.19850509@gmail.com>
2020-01-29 21:00:23 +0800

    add channel create

--- Gondwana/channel.py
+++ Gondwana/channel.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from flask import Blueprint, render_template, redirect, url_for, request, current_app
+from Gondwana.model import db, Channel
 
 bp = Blueprint('channel', __name__, url_prefix='/channel')
 
@@ -20,6 +21,29 @@ def channel_index():
 def channel_create():
     if request.method == 'POST':
         current_app.logger.debug('create channel')
+
+        # get parameters
+        name = request.form['inputName']
+        website = request.form['inputWebsite']
+        email = request.form['inputEmail']
+        apiKey = request.form['inputApikey']
+        current_app.logger.debug('name: %s' % (name))
+        current_app.logger.debug('website: %s' % (website))
+        current_app.logger.debug('email: %s' % (email))
+        current_app.logger.debug('apiKey: %s' % (apiKey))
+
+        # create Channel object
+        channel = Channel(name, website, email, apiKey)
+        current_app.logger.debug('channel: %s/%s/%s/%s' % (
+            channel.name,
+            channel.website_url,
+            channel.email,
+            channel.api_key,
+        ))
+
+        # save to database
+        db.session.add(channel)
+        db.session.commit()
 
         return redirect(url_for('channel.channel_index'))
 
