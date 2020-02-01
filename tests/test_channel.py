4144970b8b9a9794b8260780a86f4c027980512c
Richard Ma <richard.ma.19850509@gmail.com>
2020-02-01 23:02:14 +0800

    add

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -10,3 +10,16 @@ def test_empty_database(client):
     assert 'No channels here.' in response_data
     # confirm Create Channel Button
     assert 'Create Channel</a>' in response_data
+
+def test_channel_create_form(client):
+    response = client.get('/channel/create')
+    response_data = str(response.data)
+
+    assert response.status_code == 200
+
+    # confirm form
+    assert 'inputName' in response_data
+    assert 'inputWebsite' in response_data
+    assert 'inputEmail' in response_data
+    assert 'inputApikey' in response_data
+    assert '<button type="submit" class="btn btn-primary">Save</button>' in response_data
