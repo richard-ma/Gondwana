7e9b61d0e45bd1381e526014a03aea72e0e0d7bc
Richard Ma <richard.ma.19850509@gmail.com>
2020-02-01 23:10:45 +0800

    add test_create_channel

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -3,23 +3,46 @@
 
 def test_empty_database(client):
     response = client.get('/channel/index')
-    response_data = str(response.data)
 
     assert response.status_code == 200
     # confirm message
-    assert 'No channels here.' in response_data
+    assert b'No channels here.' in response.data
     # confirm Create Channel Button
-    assert 'Create Channel</a>' in response_data
+    assert b'Create Channel</a>' in response.data
 
 def test_channel_create_form(client):
     response = client.get('/channel/create')
-    response_data = str(response.data)
 
     assert response.status_code == 200
 
     # confirm form
-    assert 'inputName' in response_data
-    assert 'inputWebsite' in response_data
-    assert 'inputEmail' in response_data
-    assert 'inputApikey' in response_data
-    assert '<button type="submit" class="btn btn-primary">Save</button>' in response_data
+    assert b'inputName' in response.data
+    assert b'inputWebsite' in response.data
+    assert b'inputEmail' in response.data
+    assert b'inputApikey' in response.data
+    assert b'<button type="submit" class="btn btn-primary">Save</button>' in response.data
+
+def test_create_channel(client):
+    # test channel
+    test_channel_data = dict(
+        inputName='test_channel_name',
+        inputWebsite='http://test_channel_website.com',
+        inputEmail='test@test_channel.com',
+        inputApikey='test_channel_api_key',
+    )
+
+    response = client.post(
+            '/channel/create',
+            data=test_channel_data,
+            follow_redirects=True
+            )
+
+    # confirm channel information
+    assert bytes(test_channel_data['inputName'], encoding='utf8') in response.data
+    assert bytes(test_channel_data['inputWebsite'], encoding='utf8') in response.data
+    assert bytes(test_channel_data['inputEmail'], encoding='utf8') in response.data
+    assert bytes(test_channel_data['inputApikey'], encoding='utf8') in response.data
+
+    # confirm operation button
+    assert b'Edit</a>' in response.data
+    assert b'Delete</a>' in response.data
