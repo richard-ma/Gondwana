34d57ed677f22ba5259f126812c9fa452108402f
Richard Ma <richard.ma.19850509@gmail.com>
2020-03-03 03:30:43 +0800

    add auto migrate script

--- run-development.sh
+++ run-development.sh
@@ -1,2 +1,8 @@
+# generate migrate script
+./manage.py db migrate
+
+# upgrade database
+./manage.py db upgrade
+
 # run server as development
 FLASK_ENV=development ./manage.py runserver
--- run-production.sh
+++ run-production.sh
@@ -1,2 +1,8 @@
+# generate migrate script
+./manage.py db migrate
+
+# upgrade database
+./manage.py db upgrade
+
 # run server as production
 ./manage.py runserver
