cc382d8fb1b7eb5d57396287f2a9dd41124ae860
Richard Ma <richard.ma.19850509@gmail.com>
2020-03-03 03:10:04 +0800

    add migrate command to manager as "db"

--- manage.py
+++ manage.py
@@ -3,12 +3,14 @@
 
 import pytest
 from flask_script import Manager
+from flask_migrate import MigrateCommand
 from Gondwana import create_app
 
 app = create_app()
 
 # initial manager instance
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
