8edae681bdac7a44b18ab6703c75f4f3ecf22c9e
Richard Ma <richard.ma.19850509@gmail.com>
2020-03-03 06:20:44 +0800

    move BASEDIR to config file

--- manage.py
+++ manage.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import os
 import pytest
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 from Gondwana import create_app
 
-
 app = create_app()
-app.config['BASEDIR'] = os.path.abspath(os.path.dirname(__file__))
 
 # initial manager instance
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def test():
     pytest.main(["-s", "tests"])
+
 
 if __name__ == "__main__":
     manager.run()
