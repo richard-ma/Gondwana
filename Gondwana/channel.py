a7658a0a88f2973858462a9862d6311d58f581ad
Richard Ma <richard.ma.19850509@gmail.com>
2020-03-03 20:56:32 +0800

    remove sql paramaters log

--- Gondwana/channel.py
+++ Gondwana/channel.py
@@ -19,26 +19,14 @@ def channel_index():
 @bp.route('/create', methods=('GET', 'POST'))
 def channel_create():
     if request.method == 'POST':
-        current_app.logger.debug('create channel')
-
         # get parameters
         name = request.form['inputName']
         website = request.form['inputWebsite']
         email = request.form['inputEmail']
         apiKey = request.form['inputApikey']
-        current_app.logger.debug('name: %s' % (name))
-        current_app.logger.debug('website: %s' % (website))
-        current_app.logger.debug('email: %s' % (email))
-        current_app.logger.debug('apiKey: %s' % (apiKey))
 
         # create Channel object
         channel = Channel(name, website, email, apiKey)
-        current_app.logger.debug('channel: %s/%s/%s/%s' % (
-            channel.name,
-            channel.website_url,
-            channel.email,
-            channel.api_key,
-        ))
 
         # save to database
         db.session.add(channel)
@@ -52,11 +40,8 @@ def channel_create():
 # /channel/delete/<str:id>
 @bp.route('/delete/<string:id>', methods=('GET',))
 def channel_delete(id: str):
-    current_app.logger.debug("Delete #%s Channel" % (id))
-
     # query the record
     channel = Channel.query.filter_by(id=id).first()
-    current_app.logger.debug("Queried Channel id: %s" % (id))
 
     # save to database
     db.session.delete(channel)
@@ -69,20 +54,14 @@ def channel_delete(id: str):
 @bp.route('/update/<string:id>', methods=('GET', 'POST'))
 def channel_update(id: str):
     channel = Channel.query.filter_by(id=id).first()
-    current_app.logger.debug("Queried Channel id: %s" % (channel.id))
 
     if request.method == 'POST':
-        current_app.logger.debug('update channel #%s' % (channel.id))
 
         # get parameters
         channel.name = request.form['inputName']
         channel.website_url = request.form['inputWebsite']
         channel.email = request.form['inputEmail']
         channel.api_key = request.form['inputApikey']
-        current_app.logger.debug('name: %s' % (channel.name))
-        current_app.logger.debug('website: %s' % (channel.website_url))
-        current_app.logger.debug('email: %s' % (channel.email))
-        current_app.logger.debug('apiKey: %s' % (channel.api_key))
 
         db.session.commit()
 
