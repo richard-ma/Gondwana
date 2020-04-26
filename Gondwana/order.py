89d8a45bfec1b7c1bf2859954504c2b81da45d47
Richard Ma <richard.ma.19850509@gmail.com>
2020-04-26 19:48:45 +0800

    response xlsx file

--- Gondwana/order.py
+++ Gondwana/order.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
 from datetime import datetime
-from flask import Blueprint, render_template, redirect, url_for, request, current_app, flash
+from flask import Blueprint, render_template, redirect, url_for, request, current_app, flash, send_file
 from Gondwana.model import *
 from Gondwana.helper import *
 from pycscart import Cscart
@@ -16,21 +17,34 @@ bp = Blueprint('order', __name__, url_prefix='/order')
 def order_download():
     download = request.form.get('download')
 
-    if download: # download order
+    if download:  # download order
         order_ids = request.form.get('order_ids')
         if order_ids:
-            order_ids = [int(x) for x in order_ids.split(',')] # change string to integer
+            order_ids = [int(x) for x in order_ids.split(',')
+                         ]  # change string to integer
         # https://stackoverflow.com/questions/15267755/query-for-multiple-values-at-once
         batching_orders = Order.query.filter(Order.id.in_(order_ids))
 
-    return str([order.id for order in batching_orders])
+    import xlsxwriter
+
+    xlsx_filename = os.path.join(current_app.instance_path,
+                                 current_app.config['ORDER_DOWNLOAD_FILENAME'])
+    workbook = xlsxwriter.Workbook(xlsx_filename)
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write(0, 0, 'helo')
+
+    workbook.close()
+
+    # https://stackoverflow.com/questions/50086585/download-file-from-root-directory-using-flask
+    return send_file(xlsx_filename, as_attachment=True)
 
 
 # /order/index
 @bp.route('/index', methods=('GET', 'POST'))
 def order_index():
-    channels = Channel.query.all() # get all channels
-    events = Event.query.all() # get all events
+    channels = Channel.query.all()  # get all channels
+    events = Event.query.all()  # get all events
     orders_query = db.session.query(Order)  # create Order Query
     active_channel = None
     active_status = None
@@ -50,27 +64,30 @@ def order_index():
         # search
         if channel_id:  # Add conditions
             orders_query = orders_query.filter(Order.channel_id == channel_id)
-            active_channel = Channel.query.filter(Channel.id == channel_id).first()
+            active_channel = Channel.query.filter(
+                Channel.id == channel_id).first()
         if status:  # status filter
             orders_query = orders_query.filter(Order.status == status)
             active_status = status
-        if event_id: # event filter
+        if event_id:  # event filter
             # https://stackoverflow.com/questions/6474989/sqlalchemy-filter-by-membership-in-at-least-one-many-to-many-related-table
-            orders_query = orders_query.filter(Order.events.any(Event.id.in_([event_id])))
+            orders_query = orders_query.filter(
+                Order.events.any(Event.id.in_([event_id])))
             active_event = Event.query.filter(Event.id == event_id).first()
-        if keyword: # search keyword
-            orders_query = orders_query.filter(or_(Order.order_id == keyword, Order.email == keyword))
+        if keyword:  # search keyword
+            orders_query = orders_query.filter(
+                or_(Order.order_id == keyword, Order.email == keyword))
         # batch
-        if status_to: # change order status
+        if status_to:  # change order status
             order_ids = request.form.get('order_ids')
             if order_ids:
-                order_ids = [int(x) for x in order_ids.split(',')] # change string to integer
+                order_ids = [int(x) for x in order_ids.split(',')
+                             ]  # change string to integer
             # https://stackoverflow.com/questions/15267755/query-for-multiple-values-at-once
             batching_orders = Order.query.filter(Order.id.in_(order_ids))
             for order in batching_orders:
                 order.status = status_to
             db.session.commit()
-
 
     orders = orders_query.all()  # execute Query
 
@@ -84,12 +101,14 @@ def order_index():
                            active_event=active_event,
                            keyword=keyword)
 
+
 # /order/detail
 @bp.route('/detail/<string:order_id>', methods=('GET', ))
 def order_detail(order_id: str):
-    order = Order.query.filter(Order.id==order_id).first()
+    order = Order.query.filter(Order.id == order_id).first()
 
     return render_template('/order/detail.html', order=order)
+
 
 # /order/sync
 @bp.route('/sync', methods=('GET', ))
@@ -102,17 +121,23 @@ def order_sync():
         for o in response['orders']:
             order_id = o['order_id']
             order_remote = api.get_order(order_id)
-            order_local = Order.query.filter(Order.order_id == order_id).first()
+            order_local = Order.query.filter(
+                Order.order_id == order_id).first()
 
-            order_remote['shipping_method'] = order_remote['shipping'][0]['shipping'] # shipping method
+            order_remote['shipping_method'] = order_remote['shipping'][0][
+                'shipping']  # shipping method
 
             # choose keys in order_remote
-            order_remote = {k:v for k, v in order_remote.items() if k in order_keys}
-            order_remote['channel'] = channel # add foreign key
+            order_remote = {
+                k: v
+                for k, v in order_remote.items() if k in order_keys
+            }
+            order_remote['channel'] = channel  # add foreign key
 
             # timestamp to datetime
             # https://stackoverflow.com/questions/3682748/converting-unix-timestamp-string-to-readable-date
-            order_remote['timestamp'] = datetime.utcfromtimestamp(int(order_remote['timestamp']))
+            order_remote['timestamp'] = datetime.utcfromtimestamp(
+                int(order_remote['timestamp']))
 
             if order_local:
                 for k, v in order_remote.items():
